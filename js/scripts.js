782301e186bd49ad9e10d9a8d10a72fdb28e396c
srhhttnr <shuettner12@gmail.com>
2024-01-10 10:08:58 -0600

    fixed accidental capitalization on variable name

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {Name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
     {name: 'Squirtle', height: 5, type: ['water']},
     {name: 'Jigglypuff', height: 5, type: ['fairy', 'normal']},
     {name: 'Eevee', height: 5, type: ['normal']},
