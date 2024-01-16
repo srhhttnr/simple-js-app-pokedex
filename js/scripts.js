990877333463755b00dd9ea51747d98f97904689
srhhttnr <shuettner12@gmail.com>
2024-01-16 16:02:32 -0600

    created IIFE around array and functions for array

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
-// array with various Pokemon, their height, and their type
-let pokemonList = [
-    {name: 'Bulbasaur', height: .7, type: ['grass', 'poison']},
-    {name: 'Squirtle', height: .5, type: ['water']},
-    {name: 'Jigglypuff', height: .5, type: ['fairy', 'normal']},
-    {name: 'Eevee', height: .5, type: ['normal']},
-    {name: 'Pikachu', height: .4, type: ['electric']}
-];
+// IIFE around array with Pokemon List
+let pokemonRepository = (function () {
+    let pokemonList = [
+      { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+      { name: "Squirtle", height: 0.5, type: ["water"] },
+      { name: "Jigglypuff", height: 0.5, type: ["fairy", "normal"] },
+      { name: "Eevee", height: 0.5, type: ["normal"] },
+      { name: "Pikachu", height: 0.4, type: ["electric"] },
+    ]
+  
+    // function to return list of pokemon
+    function getAll() {
+      return pokemonList;
+    }
 
-//forEach loop that writes their name and details
-pokemonList.forEach(function(pokemon) {
-    document.write("<p>" + `${pokemon.name}; ${pokemon.height}m; ${pokemon.type}` + "</p>");
-})
+    // function that adds pokemon to the array
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
 
-// for loop that writes their name and height
-// for (let i = 0; i < pokemonList.length; i++){
-//     document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}m)`);
-
-//     if (pokemonList[i].height > .5){
-//         document.write(`~~Wow that's a big Pokemon!~~` + "<br>");
-//     } else if (pokemonList[i].height < .5){
-//         document.write(`~~Wow that's a small Pokemon!~~` + "<br>");
-//     } else {
-//         document.write("<br>");
-//     }
-// }
+    return {
+      getAll: getAll,
+      add: add,
+    }
+})();
+  
+// print all pokemon in the list
+document.write(pokemonRepository.getAll());
