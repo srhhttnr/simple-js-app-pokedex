3e83a5d0e5e564e21a0ac7f5a8c97efb52afe52d
srhhttnr <shuettner12@gmail.com>
2024-01-16 16:56:57 -0600

    added conditionals for printing pokemon, height, and statements if considered large/small, added conditional to add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 // IIFE around array with Pokemon List
 let pokemonRepository = (function () {
     let pokemonList = [
-      { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
-      { name: "Squirtle", height: 0.5, type: ["water"] },
-      { name: "Jigglypuff", height: 0.5, type: ["fairy", "normal"] },
-      { name: "Eevee", height: 0.5, type: ["normal"] },
-      { name: "Pikachu", height: 0.4, type: ["electric"] },
+      { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+      { name: 'Squirtle', height: 0.5, type: ['water'] },
+      { name: 'Jigglypuff', height: 0.5, type: ['fairy', 'normal'] },
+      { name: 'Eevee', height: 0.5, type: ['normal'] },
+      { name: 'Pikachu', height: 0.4, type: ['electric'] },
     ]
-  
+    
+    // function that adds pokemon to the array
+    function add(pokemon) {
+      if (typeof pokemon === 'object') {
+        pokemonList.push(pokemon);
+      }
+    }
+
     // function to return list of pokemon
     function getAll() {
       return pokemonList;
-    }
-
-    // function that adds pokemon to the array
-    function add(pokemon) {
-      pokemonList.push(pokemon);
     }
 
     return {
@@ -24,5 +26,17 @@ let pokemonRepository = (function () {
     }
 })();
   
+pokemonRepository.add({
+  name: 'Pidgey', height: 0.3, type: ['flying', 'normal']
+});
+
 // print all pokemon in the list
-document.write(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(function(pokemon){
+  if (pokemon.height > 0.5) {
+    document.write('<p>', pokemon.name + ': ' + pokemon.height + ' m;' + ' Wow, that\'s a big pokemon! </p>');
+  } else if (pokemon.height < 0.5) {
+    document.write('<p>', pokemon.name + ': ' + pokemon.height + ' m;' + ' Wow, that\'s a small pokemon! </p>');
+  } else {
+    document.write('<p>', pokemon.name + ': ' + pokemon.height + ' m;' + '</p>');
+  }
+});
