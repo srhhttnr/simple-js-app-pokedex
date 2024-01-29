d92a8f72fa59c72125c3b6be520660462b574104
srhhttnr <shuettner12@gmail.com>
2024-01-29 13:35:58 -0600

    added loader until promise is resolved or rejected for loadDetails() and loadList() functions

--- js/scripts.js
+++ js/scripts.js
@@ -43,11 +43,23 @@ let pokemonRepository = (function () {
       });
     }
 
+    // function that shows loading message while fetching data
+    function showLoadingMessage () {
+      loader.classList.add("display");
+    }
+
+    // function that hides loading message after data is fetched
+    function hideLoadingMessage () {
+      loader.classList.remove("display");
+    }
+
     // function that loads each pokemon from the pokemon API
     function loadList() {
+      showLoadingMessage()
       return fetch(apiUrl).then(function (response) {
         return response.json();
       }).then(function (json) {
+        hideLoadingMessage()
         json.results.forEach(function (item) {
           let pokemon = {
             name: item.name,
@@ -57,21 +69,25 @@ let pokemonRepository = (function () {
           console.log(pokemon);
         });
       }).catch(function (e) {
+        hideLoadingMessage()
         console.error(e);
       })
     }
 
     // function that loads details of pokemon
     function loadDetails(item) {
+      showLoadingMessage()
       let url = item.detailsUrl;
       return fetch(url).then(function (response) {
         return response.json();
       }).then(function (details) {
+        hideLoadingMessage()
         // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
         item.types = details.types;
       }).catch(function (e) {
+        hideLoadingMessage()
         console.error(e);
       });
     }
