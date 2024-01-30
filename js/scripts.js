adfc606fdd362596997e827eb2c9b018b40d5b77
srhhttnr <shuettner12@gmail.com>
2024-01-30 12:56:10 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -130,7 +130,7 @@ let pokemonRepository = (function () {
           pokemonTypes = pokemon.type.name;
           
           if (pokemon.type.id > 0) {
-            pokemonTypes = [`${pokemon.type.name.id=0}, ${pokemon.type.name.id=1}, ${pokemon.type.name.id=2}`];
+            pokemonTypes = [`${pokemon.type.name}, ${pokemon.type.name}, ${pokemon.type.name}`];
           }
         };
 
